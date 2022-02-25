bdc56392e707fedcb9a727ad95f9c80c1718fa63
bcrich714 <brody.crichw@gmail.com>
2022-02-25 15:31:29 +1100

    Added project files

---
+++ Handymans Toolkit Mobile App/HandymansToolkit/HandymansToolkit/Views/HomePage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HandymansToolkit.Views
+{
+    public partial class HomePage : ContentPage
+    {
+        //Constructor/Initializer
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        //List of re-directions for the front-end buttons.
+        private async void FlashlightClicked(Object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//Torch");
+        }
+        private async void SpiritLevelClicked(Object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//SpiritLevel");
+        }
+
+        private async void FloorPlannerClicked(Object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//FloorPlanner");
+        }
+
+        private async void TaskPlannerClicked(Object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//TaskPlanner");
+        }
+
+    }
+}
---
+++ Handymans Toolkit Mobile App/HandymansToolkit/HandymansToolkit/Views/Torch.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace HandymansToolkit.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Torch : ContentPage
+    {
+        //Variables
+        bool isOn = false;
+        private string buttonText;
+
+        //Getter/Setter for button text
+        public string ButtonText
+        {
+            get { return buttonText; }
+            set
+            {
+                buttonText = value;
+                OnPropertyChanged(nameof(ButtonText));
+            }
+        }
+
+        //Constructor/Initializer
+        public Torch()
+        {
+            ButtonText = "Flash OFF";
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        //Whenever the button is clicked, switch the state of the flashlight and change the button text
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            try
+            {
+                // Turn On/Off Flashlight, change button text for toggle effect.
+                if (isOn)
+                {
+                    isOn = !isOn;
+                    await Flashlight.TurnOffAsync();
+                    ButtonText = "Flash OFF";
+                }
+                else
+                {
+                    isOn = !isOn;
+                    await Flashlight.TurnOnAsync();
+                    ButtonText = "Flash ON";
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
